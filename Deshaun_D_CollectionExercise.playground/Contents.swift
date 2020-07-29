c19b7c55f09ef27c45004975db6fdc6f29b480dc
dndouglas1 <67969248+dndouglas1@users.noreply.github.com>
2020-07-29 07:40:20 -0500

    Add files via upload

---
+++ Deshaun_D_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,11 @@
+var someInts = [Int]()
+
+someInts.append(10)
+
+var favVideoGames: [String] = ["Apex Legends","Assassin's Creed","Hyper Scape", "Brawlhalla", "Roblox","Paladins","Call of Duty","Spongebob: Rehydrated","Super Smash Bros", "Mario Kart"]
+
+print("The video games list contains \(favVideoGames.count) items.")
+
+favVideoGames.sort()
+print(favVideoGames)
+
